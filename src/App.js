78b982d15681e8d0eace206b2739a883525d2d17
Rein Chau <rein.chau@firstidea.vn>
2019-12-07 00:18:18 +0700

    Lesson 03: Loading component from external JS file.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+const e = React.createElement
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class HelloComponent extends React.Component {
+	render() {
+		const h3Style = { color: 'blue' }
+		const h4Style = { 'text-align': 'right' }
+		return (
+			<div className='alert alert-success' role='alert'>
+				<h3 style={h3Style}>Hello</h3>
+				<h4 style={h4Style}>CodeSchool</h4>
+			</div>
+		)
+	}
 }
 
-export default App;
+class ContainerComponent extends React.Component {
+	render() {
+		return e('div', { className: 'container' },
+			e('div', { className: 'card' },
+				e('div', { className: 'card-header' },
+					'React Example - by CodeSchool.VN'
+				),
+				e('div', { className: 'card-body' },
+					e(HelloComponent)
+				),
+			)
+		)
+	}
+}
+
+window.ContainerComponent = ContainerComponent
